baf5122288a091fbdc90eb4cfc169e3ca360b3d5
romashka77 <romashka_77@mail.ru>
2018-08-24 11:11:55 +0300

    Добавьте файлы проекта.

---
+++ OnePage/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using OnePage.Models;
+
+namespace OnePage.Controllers
+{
+    public class HomeController : Controller
+    {
+        ReservationRepository repository = ReservationRepository.Current;
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View(repository.GetAll());
+        }
+
+        public ActionResult Add(Reservation item)
+        {
+            if (ModelState.IsValid)
+            {
+                repository.Add(item);
+                return RedirectToAction("Index");
+            }
+            else return View("Index");
+        }
+
+        public ActionResult Update(Reservation item)
+        {
+            if (ModelState.IsValid && repository.Update(item))
+                return RedirectToAction("Index");
+            else return View("Index");
+        }
+    }
+}
